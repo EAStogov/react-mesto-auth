5da26a188bcffd6cc512bf949b5af506c3aedfe4
EgorStogov <egst94@yandex.ru>
2022-05-15 12:27:59 +0300

    feature: create ProtectedRoute

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,12 +1,8 @@
 import React from "react";
-import { Route, Redirect} from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({component: Component, ...props}) => {
-  return (
-    <Route>
-      {() => props.loggedIn ? <Component/> : <Redirect to="./sign-in"/>}
-    </Route>
-  );
+  return (props.loggedIn ? <Component/> : <Navigate to="./sign-in"/>);
 }
 
 export default ProtectedRoute;
